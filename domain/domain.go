a9048fa4e89348b443e76cb32307d66afa487b3a
Naveen Prakash <naveenprakashjk24@gmail.com>
2022-02-13 14:40:35 +0530

    initial commit

---
+++ domain/domain.go
@@ -0,0 +1,140 @@
+package domain
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type InputData struct {
+	Ev     string `json:"ev"`
+	Et     string `json:"et"`
+	Id     string `json:"id"`
+	Uid    string `json:"uid"`
+	Mid    string `json:"mid"`
+	T      string `json:"t"`
+	P      string `json:"p"`
+	L      string `json:"l"`
+	Sc     string `json:"sc"`
+	Atrk1  string `json:"atrk1"`
+	Atrv1  string `json:"atrv1"`
+	Atrt1  string `json:"atrt1"`
+	Atrk2  string `json:"atrk2"`
+	Atrv2  string `json:"atrv2"`
+	Atrt2  string `json:"atrt2"`
+	Uatrk1 string `json:"uatrk1"`
+	Uatrv1 string `json:"uatrv1"`
+	Uatrt1 string `json:"uatrt1"`
+	Uatrk2 string `json:"uatrk2"`
+	Uatrv2 string `json:"uatrv2"`
+	Uatrt2 string `json:"uatrt2"`
+	Uatrk3 string `json:"uatrk3"`
+	Uatrv3 int    `json:"uatrv3"`
+	Uatrt3 string `json:"uatrt3"`
+}
+
+type OutputData struct {
+	Event            string `json:"even"`
+	Event_type       string `json:"event_type"`
+	App_id           string `json:"app_id"`
+	User_id          string `json:"user_id"`
+	Message_id       string `json:"message_id"`
+	Page_title       string `json:"page_title"`
+	Page_url         string `json:"page_url"`
+	Browser_language string `json:"browser_language"`
+	Screen_size      string `json:"screen_size"`
+	Attributes       struct {
+		Form_varient struct {
+			Value string `json:"value"`
+			Type  string `json:"type"`
+		} `json:"form_varient"`
+		Ref struct {
+			Value string `json:"value"`
+			Type  string `json:"type"`
+		} `json:"ref"`
+	} `json:"attributes"`
+	Traits struct {
+		Name struct {
+			Value string `json:"value"`
+			Type  string `json:"type"`
+		} `json:"name"`
+		Email struct {
+			Value string `json:"value"`
+			Type  string `json:"type"`
+		} `json:"email"`
+		Age struct {
+			Value int    `json:"value"`
+			Type  string `json:"type"`
+		} `json:"age"`
+	} `json:"traits"`
+}
+
+func NewWorker(jobs <-chan InputData, results chan<- OutputData) {
+	for input := range jobs {
+
+		r := OutputData{
+			Event:            input.Ev,
+			Event_type:       input.Et,
+			App_id:           input.Id,
+			User_id:          input.Uid,
+			Message_id:       input.Mid,
+			Page_title:       input.T,
+			Page_url:         input.P,
+			Browser_language: input.L,
+			Screen_size:      input.Sc,
+			// Attributes {
+			// 	Form_varient : InputData.atrk1  {
+			// 		Value : inputData.atrv1,
+			// 		Type  : InputData.atrt1
+			// 	},
+			// 	Ref  {
+			// 		Value : InputData.atrv2,
+			// 		Type  : InputData.atrt2
+			// 	}
+			// } : "attributes",
+			// Traits {
+			// 	Name :InputData.uatrk1 {
+			// 		Value : InputData.uatrv1,
+			// 		Type  : InputData.uatrt1
+			// 	},
+			// 	Email :InputData.uatrk2 {
+			// 		Value : InputData.uatrv2,
+			// 		Type  : InputData.uatrt2
+			// 	},
+			// 	Age : InputData.uatrk3 {
+			// 		Value : InputData.uatrv3,
+			// 		Type  : InputData.uatrt3
+			// 	},
+			// }
+		}
+		results <- r
+	}
+}
+func ProcessJsonInput(inputuserData []byte) []InputData {
+	collection := []InputData{}
+	var data map[string][]json.RawMessage
+	err := json.Unmarshal(inputuserData, &data)
+	if err != nil {
+		log.Println(err)
+		return collection
+	}
+	for _, thing := range data["userdetails"] {
+		collection = addInput(thing, collection)
+
+	}
+	return collection
+}
+
+func addInput(thing json.RawMessage, collection []InputData) []InputData {
+	input := InputData{}
+	err := json.Unmarshal(thing, &input)
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		if input != *new(InputData) {
+			collection = append(collection, input)
+		}
+	}
+
+	return collection
+}
---
+++ server/server.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type UserInput struct{}
+
+func (handler *UserInput) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 2048666))
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	inputCollection := domain.ProcessJsonInput(body)
+	if len(inputCollection) <= 0 {
+		http.Error(w, "{\"error\": \"empty input list submitted.\"}", 500)
+		return
+	}
+
+	inputMap := make(map[int]domain.InputData)
+	for k, v := range inputCollection {
+		inputMap[k] = v
+	}
+	//The number of jobs we have to execute is the count of elements in our input map
+	numberOfJobs := len(inputMap)
+	numberOfWorkers := 100
+
+	//Buffered chanels
+	jobs := make(chan domain.InputData, 1000)     //Chanel to send in the jobs
+	results := make(chan domain.OutputData, 1000) //Chanel to receive job results
+
+	for w := 1; w <= numberOfWorkers; w++ {
+		go domain.NewWorker(jobs, results)
+	}
+
+	for j := 0; j <= numberOfJobs-1; j++ {
+		job := inputMap[j]
+		jobs <- job
+	}
+	//close the jobs channel
+	close(jobs)
+
+	//The res map will be used to collect results of our workers...
+	res := make(map[int]domain.OutputData)
+
+	//Collect worker results
+	for a := 0; a <= numberOfJobs-1; a++ {
+		r := <-results
+		res[r.Index] = r
+	}
+	close(results)
+
+	//Set response header information
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+
+	//Create json with all results...
+	response, err := domain.GenerateJsonOutput(res)
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(response))
+
+}
+
+func main() {
+	//The port our server will be litening on
+	port := 8080
+
+	//Our fancy Gorilla mux router
+	router := mux.NewRouter()
+
+	//Our Routes :
+	//We're only interested in POST requests on /calcs URL
+	router.Handle("/userdetails", &UserInput{}).Methods("POST")
+
+	log.Printf("Starting server. Listening on port %d", port)
+	err := http.ListenAndServe(":"+strconv.Itoa(port), router)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
